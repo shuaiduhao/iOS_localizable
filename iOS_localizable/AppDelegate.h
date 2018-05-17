5f71fac302df07f8e32d67f9f2be51c99a921b7a
ds.sunagy <user@sunagyappledeveloperdemac-mini.local>
2018-05-17 16:24:51 +0800

    Initial Commit

---
+++ iOS_localizable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_localizable
+//
+//  Created by ds.sunagy on 2018/5/17.
+//  Copyright © 2018年 ds.sunagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
